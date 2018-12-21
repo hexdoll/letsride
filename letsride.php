3c91349aede352352871cf6fa4b80bb375123008
Gemma Peter <me@gemmapeter.co.uk>
2018-12-21 21:01:26 +0000

    Change it all to OOP
    Basic plugin that can install and uninstall itself
    Plugin can create the table it needs to store feed data
    Plugin drops the table when it uninstalls itself

--- letsride.php
+++ letsride.php
@@ -1,8 +1,4 @@
 <?php
-/**
- * @package Lets_Ride
- * @version 0.1
- */
 /*
 Plugin Name: Let's Ride
 Description: Demo Project for Big Wave Media.
@@ -11,103 +7,122 @@ Version: 0.1
 Author URI: http://gemmapeter.co.uk/
 */
 
-$letsride_db_version = '0.1';
-$letsride_db_table = 'letsride';
-
-/*
- * Install the plugin
- */
-register_activation_hook(__FILE__, 'letsride_options_init');
-register_activation_hook(__FILE__, 'letsride_db_init');
-register_activation_hook(__FILE__, 'letsride_cron_start');
-
-/*
- * Initialise the wp options for plugin
- */
-function letsride_options_init()
+class LetsRide
 {
-	//Google maps API credentials
-	//plugin db version
+	const DB_VERSION = '0.1';
+	const TABLE = 'letsride';
+	const PREFIX = 'letsride_';
+
+	public static function register() {
+
+		register_activation_hook(__FILE__, [__CLASS__, 'activate']);
+		register_deactivation_hook(__FILE__, [__CLASS__, 'deactivate']);
+		register_uninstall_hook(__FILE__, [__CLASS__, 'uninstall']);
+	}
+
+	/*
+	 * Install the plugin
+	 */
+	public static function activate() {
+		self::db_init();
+		self::options_init();
+		self::cron_start();
+	}
+
+	/*
+	 * Deactivate the plugin
+	 */
+	public static function deactivate() {
+		self::cron_stop();
+	}
+
+	public static function uninstall() {
+		self::db_delete();
+		self::options_delete();
+	}
+
+	/*
+	 * Initialise the wp options for plugin
+	 */
+	public static function options_init()
+	{
+		//Google maps API credentials
+		//plugin db version
+	}
+
+	/*
+	 * Remove plugin's wp options entries
+	 */
+	public static function options_delete()
+	{
+		//TODO: Write me
+	}
+
+	/*
+	 * Initialise the db table for storage of feed data
+	 */
+	public static function db_init()
+	{
+		// https://codex.wordpress.org/Creating_Tables_with_Plugins
+		global $wpdb;
+		$table = $wpdb->prefix.self::TABLE;
+		$charset_collate = $wpdb->get_charset_collate();
+
+		$sql = "CREATE TABLE $table (
+			id INTEGER NOT NULL AUTO_INCREMENT,
+			feed_url VARCHAR(255) NOT NULL,
+			title VARCHAR(255),
+			description TEXT,
+			date DATETIME NOT NULL,
+			location TEXT,
+			thumbnail VARCHAR(255),
+			url VARCHAR(255),
+			PRIMARY KEY (id)
+			) $charset_collate;";
+
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+		dbDelta($sql);
+
+		add_option(self::PREFIX.'db_version', self::DB_VERSION);
+	}
+
+	/*
+	 * Cleanly removes table from database
+	 */
+	public static function db_delete()
+	{
+		global $wpdb;
+		$table = $wp_db->prefix.self::TABLE;
+		$wpdb->query("DROP TABLE IF EXISTS $table;");
+		delete_option(self::PREFIX.'db_version');
+	}
+
+	/*
+	 * Starts the cron to bring in the data
+	 */
+	public static function cron_start()
+	{
+		//TODO: write me
+	}
+
+	/*
+	 * Stops the cron to bring in the data
+	 */
+	public static function cron_stop()
+	{
+		//TODO: write me
+	}
+
+	/*
+	 * Returns the places to load data from
+	 * @return array of feeds
+	 */
+	public static function active_feed_urls()
+	{
+		return array(
+			"http://api.letsride.co.uk/public/v1/rides",
+		);
+	}
 }
 
-/*
- * Initialise the db table for storage of feed data
- */
-function letsride_db_init()
-{
-	// https://codex.wordpress.org/Creating_Tables_with_Plugins
-	global $wpdb;
-	global $letsride_db_table;
-	$table = $wpdb->prefix . $letsride_db_table;
-	$charset_collate = $wpdb->get_charset_collate();
-
-	$sql = "CREATE TABLE $table
-		id INT NOT NULL AUTO_INCREMENT,
-		feed_url VARCHAR(255) NOT NULL,
-		title VARCHAR(255),
-		description TEXT,
-		date DATETIME NOT NULL,
-		location TEXT,
-		thumbnail VARCHAR(255),
-		url VARCHAR(255),
-		PRIMARY KEY (id)
-	) $charset_collate;";
-
-	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-	dbDelta($sql);
-
-	add_option('letsride_db_version', $letsride_db_version);
-}
-
-/*
- * Starts the cron to bring in the data
- */
-function letsride_cron_start()
-{
-	//TODO: write me
-}
-
-/*
- * Deactivate the plugin
- */
-register_deactivation_hook(__FILE__, 'letsride_cron_stop');
-
-/*
- * Starts the cron to bring in the data
- */
-function letsride_cron_stop()
-{
-	//TODO: write me
-}
-
-/*
- * Runs the uninstall procedure for the plugin
- */
-register_uninstall_hook(__FILE__, 'letsride_db_delete');
-register_uninstall_hook(__FILE__, 'letsride_options_delete');
-
-function letsride_db_delete()
-{
-	global $wpdb;
-	//$wpdb->
-	add_option('letsride_db_version', $letsride_db_version);
-}
-
-/*
- * Remove plugin's wp options entries
- */
-function letsride_options_delete()
-{
-	//TODO: Write me
-}
-
-/*
- * Returns the places to load data from (currently hardcoded)
- * @return array of feeds
- */
-function letsride_feed_urls()
-{
-	return array(
-		"http://api.letsride.co.uk/public/v1/rides",
-	);
-}
+LetsRide::register();
