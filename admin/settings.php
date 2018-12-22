2009390c999467da7ad0e68949328ef4f6d54b61
Gemma Peter <me@gemmapeter.co.uk>
2018-12-22 13:52:18 +0000

    List feeds on settings page

--- admin/settings.php
+++ admin/settings.php
@@ -2,6 +2,19 @@
 if (!defined('WPINC')) {
     die('Permission Denied');
 }
+$feeds = LetsRide::active_feed_urls();
 ?>
 
 <h1>Let's Ride Settings</h1>
+
+<p>Current data feeds:</p>
+
+<?php if ($feeds): ?>
+    <ul>
+    <?php foreach (LetsRide::active_feed_urls() as $feed): ?>
+        <li><?php echo esc_url($feed) ?></li>
+    <?php endforeach; ?>
+    </ul>
+<?php else: ?>
+    <p>No feeds found.</p>
+<?php endif; ?>
